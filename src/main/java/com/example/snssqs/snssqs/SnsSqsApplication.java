27fba326cbe22a448a629a6bd238b1ea8a38b395
Ding <jieding@f018984ead2d.ant.amazon.com>
2019-09-20 12:04:09 +0800

    dafadsfa

---
+++ src/main/java/com/example/snssqs/snssqs/SnsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.snssqs.snssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsSqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsSqsApplication.class, args);
+    }
+
+}
